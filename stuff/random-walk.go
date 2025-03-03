740ff6c8ba1e67a34850658277f1b9467055ae4e
Gift Mugweni <giftmugweni@gmail.com>
2025-03-04 00:57:41 +0200

    added perlin noise walker

--- stuff/random-walk.go
+++ stuff/random-walk.go
@@ -4,13 +4,23 @@ import (
 	"math/rand"
 	c "ray-random/constants"
 
+	"github.com/aquilax/go-perlin"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 var curX = c.COLS / 2
 var curY = c.ROWS / 2
 
+var pSource = rl.Vector2{X: 0, Y: 1000}
+var pPos = make([]rl.Vector2, 0)
+var p = perlin.NewPerlin(2, 2, 3, rand.Int63())
+
 func RandomWalkInit() {
+	// uniformInit()
+	perlinInit()
+}
+
+func uniformInit() {
 	for y := range c.ROWS {
 		for x := range c.COLS {
 			tiles[y*c.COLS+x] = false
@@ -21,13 +31,14 @@ func RandomWalkInit() {
 }
 
 func RandomWalkDraw() {
-	ConwayDraw()
+	// ConwayDraw()
+	perlinDraw()
 }
 
 func RandomWalkUpdate() {
 	// uniformWalkUpdate()
-	biasedUniformWalkUpdate()
-	tiles[curY*c.COLS+curX] = true
+	// biasedUniformWalkUpdate()
+	perlinWalkUpdate()
 }
 
 func uniformWalkUpdate() {
@@ -37,6 +48,7 @@ func uniformWalkUpdate() {
 	curX = int32(clamp(0, float64(c.COLS-1), float64(curX+int32(dX))))
 	curY = int32(clamp(0, float64(c.ROWS-1), float64(curY+int32(dY))))
 
+	tiles[curY*c.COLS+curX] = true
 }
 
 func biasedUniformWalkUpdate() {
@@ -69,10 +81,57 @@ func biasedUniformWalkUpdate() {
 		curY = int32(clamp(0, float64(c.ROWS-1), float64(curY+int32(dY))))
 	}
 
-	rand. 
+	tiles[curY*c.COLS+curX] = true
 }
 
 func perlinWalkUpdate() {
-	test := rl.Gen
+
+	dX := p.Noise1D(float64(pSource.X))
+	dY := p.Noise1D(float64(pSource.Y))
+
+	pSource.X += 0.01
+	pSource.Y += 0.01
+
+	dX = float64(rl.Remap(float32(dX), -1, 1, 0, float32(c.VIRTUAL_WIDTH-1)))
+	dY = float64(rl.Remap(float32(dY), -1, 1, 0, float32(c.VIRTUAL_HEIGHT-1)))
+	val := rl.Vector2{
+		X: float32(dX),
+		Y: float32(dY),
+	}
+
+	// if len(pPos) == 0 {
+	// 	dX = float64(rl.Remap(float32(dX), -1, 1, 0, float32(c.VIRTUAL_WIDTH-1)))
+	// 	dY = float64(rl.Remap(float32(dY), -1, 1, 0, float32(c.VIRTUAL_HEIGHT-1)))
+	// 	pPos = append(pPos, rl.Vector2{
+	// 		X: float32(dX),
+	// 		Y: float32(dY),
+	// 	})
+	// 	return
+	// }
+
+	// dX = float64(rl.Remap(float32(dX), -1, 1, -10, 10))
+	// dY = float64(rl.Remap(float32(dY), -1, 1, -10, 10))
+
+	// val := rl.Vector2{
+	// 	X: float32(pPos[len(pPos)-1].X) + float32(dX),
+	// 	Y: float32(pPos[len(pPos)-1].Y) + float32(dY),
+	// }
+
+	pPos = append(pPos, val)
+
+	if len(pPos) > 3000 {
+		pPos = pPos[1:]
+	}
+
 }
 
+func perlinDraw() {
+	for _, pos := range pPos {
+		rl.DrawCircle(int32(pos.X), int32(pos.Y), float32(2*c.TILE_WIDTH), rl.White)
+		rl.DrawCircleLines(int32(pos.X), int32(pos.Y), float32(2*c.TILE_WIDTH), rl.Black)
+	}
+}
+
+func perlinInit() {
+
+}
